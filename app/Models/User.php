8c169b77217fb2afb12839ecaa6526af2fdf1d35
edmarcres18 <edmarcrescencio856@gmail.com>
2025-07-11 14:44:21 +0800

    chore(dependencies): add laravel-ide-helper to development dependencies
    - Included "barryvdh/laravel-ide-helper" version 3.5 in the composer.json file to enhance IDE support for Laravel projects.
    - Updated composer.lock to reflect the addition of the new package and its dependencies.
    - Made minor adjustments to the platform and plugin API version in composer.lock for compatibility.

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,9 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * @property \Illuminate\Notifications\DatabaseNotificationCollection notifications
+ */
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
