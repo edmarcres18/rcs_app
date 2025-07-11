af4431862520d74f178fde4851b5420eccadfbac
edmarcres18 <edmarcrescencio856@gmail.com>
2025-07-11 14:45:20 +0800

    refactor(user): remove unused property documentation from User model
    - Eliminated the unused property documentation for notifications in the User model to improve code clarity and maintainability.

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,9 +10,6 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-/**
- * @property \Illuminate\Notifications\DatabaseNotificationCollection notifications
- */
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
