89cee39091d6d46f7e865a3d914e4b0687c5c30c
EDMAR <edmarcrescencio856@gmail.com>
2025-12-29 14:57:58 +0800

    Updated

--- app/Http/Controllers/WrappedController.php
+++ app/Http/Controllers/WrappedController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\UserActivity;
+use App\Models\User;
 use App\Services\UserActivityWrappedService;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
